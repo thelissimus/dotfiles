f8a45a7d679468e8150ebfae54046b292931f047
the lissimus <thelissimus@tuta.io>
2023-06-24 17:58:39 +0500

    add: configure vscodium and neovim
    CHANGES:
    * Refactor home manager configs
    
    REMOVED:
    * packages: cachix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,7 +5,7 @@
   imports =
     [
       ./hardware-configuration.nix
-      <home-manager/nixos>
+      ./home-configuration.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
@@ -54,125 +54,10 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.helix = { pkgs, ... }: {
-    home.stateVersion = config.system.stateVersion;
-    home.file."jdks/jdk8".source = pkgs.jdk8;
-    home.file."jdks/jdk17".source = pkgs.jdk17;
-    home.file."jdks/scala".source = pkgs.scala;
-
-    programs.bash.enable = true;
-    programs.vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        scalameta.metals
-        llvm-vs-code-extensions.vscode-clangd
-        editorconfig.editorconfig
-        usernamehw.errorlens
-        dbaeumer.vscode-eslint
-        tamasfe.even-better-toml
-        jdinhlife.gruvbox
-        golang.go
-        haskell.haskell
-        justusadam.language-haskell
-        christian-kohler.path-intellisense
-        alefragnani.project-manager
-        timonwong.shellcheck
-        vscodevim.vim
-        jnoortheen.nix-ide
-      ];
-    };
-
-    home.packages = with pkgs; [
-      firefox
-      telegram-desktop
-      keepassxc
-      qbittorrent
-      baobab
-      gcolor3
-      flameshot
-      qalculate-gtk
-      rename
-      rlwrap
-      ripgrep
-      fzf
-      tokei
-      tree
-      nicotine-plus
-      htop
-      nvtop
-      pfetch
-      sakura
-      insomnia
-      pinentry
-      difftastic
-      git
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.appindicator
-      # Editor
-      jetbrains.idea-community
-      # Documents
-      doxygen
-      obsidian
-      zathura
-      # Media
-      mpv
-      vlc
-      obs-studio
-      figma-agent
-      figma-linux
-      feh
-      optipng
-      peek
-      # Database
-      postgresql_15
-      # Message Queue
-      rabbitmq-server
-      # Tools
-      android-tools
-      # C
-      clang_16
-      llvmPackages_16.clang-unwrapped
-      gdb
-      tinycc
-      # Clojure
-      clojure
-      leiningen
-      # Go
-      go
-      # Haskell
-      ghc
-      ghcid
-      haskell-language-server
-      cabal-install
-      stack
-      # Java
-      maven
-      # jdk8
-      jdk17
-      # Nix
-      rnix-lsp
-      nixpkgs-fmt
-      # Node.js
-      nodejs_20
-      nodePackages.pnpm
-      # Scala
-      metals
-      dotty
-      # scala
-      sbt
-      scalafmt
-      scalafix
-      scala-cli
-    ];
-  };
-
   environment.systemPackages = with pkgs; [
     vim
     wget
     gnumake
-    neovim
     xclip
     lsof
     strace
