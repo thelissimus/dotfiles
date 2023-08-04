d6c26f0e4fe9be6eb24de55af1675cd5e35cdb04
thelissimus <thelissimus@tuta.io>
2023-08-05 03:30:44 +0500

    add & conf: wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Gruvbox Material (Gogh)",
+	font = wezterm.font("SF Mono", { weight = "Medium" }),
+	font_size = 15,
+	enable_tab_bar = false,
+	enable_scroll_bar = false,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	send_composed_key_when_left_alt_is_pressed = false,
+	send_composed_key_when_right_alt_is_pressed = true,
+}
