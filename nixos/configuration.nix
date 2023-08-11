b273f2978ead06c3751ac23e24fa44a68073e1b3
thelissimus <thelissimus@tuta.io>
2023-08-11 15:31:36 +0500

    fix: touchpad tap to click

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelParams = [
     "nvidia.NVreg_RegistryDwords=EnableBrightnessControl=1"
-    "psmouse.synaptics_intertouch=0"
   ];
   boot.supportedFilesystems = [ "ntfs" ];
 
