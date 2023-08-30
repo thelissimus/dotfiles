4d9a4cb7dacb8af1ee430454fc29d0a4523c8575
thelissimus <thelissimus@tuta.io>
2023-08-30 22:10:23 +0500

    refactor: decouple

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,12 +1,7 @@
-{ pkgs
-, home-manager
-, ...
-}: {
+{ pkgs, ... }: {
   imports =
     [
-      home-manager.nixosModules.default
       ./hardware-configuration.nix
-      ./home-configuration.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -41,6 +36,26 @@
       gnome.enable = true;
     };
     videoDrivers = [ "nvidia" ];
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    ensureDatabases = [ "helix" ];
+    ensureUsers = [
+      {
+        name = "helix";
+        ensurePermissions = {
+          "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all              trust
+      host  all all 127.0.0.1/32 trust
+      host  all all ::1/128      trust
+    '';
   };
 
   services.openssh.enable = true;
