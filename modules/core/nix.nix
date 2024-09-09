c527bf91f80a998bde8d3f046a023f07bcce0599
thelissimus <thelissimus@tuta.io>
2024-09-09 21:29:58 +0500

    add: hackage-server and iog caches

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -2,5 +2,13 @@
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
+    trusted-public-keys = [
+      "hackage-server.cachix.org-1:iw0iRh6+gsFIrxROFaAt5gKNgIHejKjIfyRdbpPYevY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    substituters = [
+      "https://hackage-server.cachix.org/"
+      "https://cache.iog.io"
+    ];
   };
 }
