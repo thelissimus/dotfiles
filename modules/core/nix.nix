145cab43a9872a910291e28734294c314e66cad2
thelissimus <thelissimus@tuta.io>
2025-04-07 10:16:43 +0500

    add: garnix cache

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -6,10 +6,12 @@
     trusted-public-keys = [
       "hackage-server.cachix.org-1:iw0iRh6+gsFIrxROFaAt5gKNgIHejKjIfyRdbpPYevY="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     substituters = [
       "https://hackage-server.cachix.org/"
       "https://cache.iog.io"
+      "https://cache.garnix.io"
     ];
   };
 }
