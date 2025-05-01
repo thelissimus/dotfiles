f6e7c6a30a2ade5c6fbf6701cd9df1629fe39de8
thelissimus <thelissimus@tuta.io>
2025-05-01 18:49:48 +0500

    block useless websites

--- machines/vega/configuration.nix
+++ machines/vega/configuration.nix
@@ -15,8 +15,7 @@
   networking.hostName = hostname;
   networking.extraHosts =
     ''
-      127.0.0.1 youtube.com
-      127.0.0.1 twitch.tv
+      127.0.0.1 www.twitch.tv
       127.0.0.1 x.com
     '';
   networking.networkmanager.enable = true;
