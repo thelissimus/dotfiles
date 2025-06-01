36213da42c8f4e0afac066326b2437c1ea0d3362
thelissimus <thelissimus@tuta.io>
2025-06-01 07:14:44 +0500

    fix: pkgs/update.sh

--- pkgs/apple-fonts/update.sh
+++ pkgs/apple-fonts/update.sh
@@ -8,7 +8,7 @@ for url in \
   https://devimages-cdn.apple.com/design/resources/download/NY.dmg; do
 
   font=$(basename $url | tr '[:upper:]' '[:lower:]' | sed 's/\.dmg//')
-  hash=$(nix hash to-sri --type sha256 $(nix-prefetch-url $url))
+  hash=$(nix hash to-sri --type sha256 "$(nix-prefetch-url $url)")
   printf '%s = { url = "%s"; hash = "%s"; };' "$font" "$url" "$hash" >>sources.nix
 done
 
