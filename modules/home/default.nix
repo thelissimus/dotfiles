a80c6a37908c0e16bcc9844e14602071cf91736d
thelissimus <thelissimus@tuta.io>
2025-06-26 11:42:40 +0500

    remove: scala and tlaplus

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -7,7 +7,6 @@
   git = import ./git.nix { inherit pkgs; };
   java = import ./java.nix { inherit pkgs; };
   neovim = import ./neovim.nix { inherit pkgs; };
-  scala = import ./scala.nix { inherit pkgs; };
   vscode = import ./vscode.nix { inherit pkgs; };
   wezterm = import ./wezterm.nix { inherit pkgs; };
   zathura = import ./zathura.nix { inherit pkgs; };
