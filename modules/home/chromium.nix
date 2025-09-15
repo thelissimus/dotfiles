3f0e2836dea9405a5eb452913a3503201741a6e8
thelissimus <thelissimus@tuta.io>
2025-09-15 06:22:14 +0500

    fix: chromium pkgs.fetchurl

--- modules/home/chromium.nix
+++ modules/home/chromium.nix
@@ -7,7 +7,7 @@
       let
         mkChromiumExtensionFor = browserVersion: { id, sha256, version }: {
           inherit id;
-          crxPath = builtins.fetchurl {
+          crxPath = pkgs.fetchurl {
             url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
             name = "${id}.crx";
             inherit sha256;
