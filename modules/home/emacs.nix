6d99a3527beef41254b8917424b74ce3371fbf84
thelissimus <thelissimus@tuta.io>
2025-12-25 08:43:23 +0500

    update(emacs): basic sanity

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -3,6 +3,15 @@
     enable = true;
     package = pkgs.emacs-macport;
     extraConfig = ''
+      (setq inhibit-startup-message t)
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)
+      (menu-bar-mode -1)
+      (set-face-attribute 'default nil :family "Mononoki" :height 150 :weight 'semibold)
+      (setq-default line-spacing 0.3)
+      (global-display-line-numbers-mode t)
+      (setq display-line-numbers-type 'relative)
+
       (setq evil-want-integration t)
       (setq evil-want-keybinding nil)
       (require 'evil)
