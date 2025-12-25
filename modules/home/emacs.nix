3ef4d1f39133111907f828e1362a8265c818e9af
thelissimus <thelissimus@tuta.io>
2025-12-25 07:36:04 +0500

    add(emacs): evil, gruvbox, magit

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -2,5 +2,20 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-macport;
+    extraConfig = ''
+      (setq evil-want-integration t)
+      (setq evil-want-keybinding nil)
+      (require 'evil)
+      (evil-mode 1)
+      (evil-collection-init)
+
+      (load-theme 'gruvbox-dark-medium t)
+    '';
+    extraPackages = epkgs: with epkgs; [
+      evil
+      evil-collection
+      gruvbox-theme
+      magit
+    ];
   };
 }
