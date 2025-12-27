e987d1488fdfba2db47ceebf65120d2ca925f2a5
thelissimus <thelissimus@tuta.io>
2025-12-28 01:54:08 +0500

    adjust(emacs): external init.el

---
+++ .config/emacs/init.el
@@ -0,0 +1,37 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-face-attribute 'default nil :family "Mononoki" :height 170 :weight 'semi-bold)
+(setq-default line-spacing 0.2)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
+(require 'nerd-icons)
+
+(require 'dashboard)
+(setq dashboard-banner-logo-title "")
+(setq dashboard-startup-banner 1)
+(setq dashboard-center-content t)
+(setq dashboard-vertically-center-content t)
+(setq dashboard-items '((recents . 5) (projects . 5) (bookmarks . 5)))
+(setq dashboard-projects-backend 'projectile)
+(setq dashboard-display-icons-p t)
+(setq dashboard-icon-type 'nerd-icons)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(dashboard-setup-startup-hook)
+
+(require 'envrc)
+(define-key envrc-mode-map (kbd "C-c e") 'envrc-command-map)
+(envrc-global-mode)
+
+(setq evil-want-keybinding nil)
+(require 'evil)
+(require 'evil-collection)
+(evil-mode 1)
+(evil-collection-init)
+
+(load-theme 'gruvbox-dark-medium t)
