e1a4c966e06271d323705aab194969450d92b92f
thelissimus <thelissimus@tuta.io>
2026-02-05 01:31:00 +0500

    adjust(git): migrate git config fully to nix

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,7 +3,35 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
+
+    ignores = [
+      ".DS_Store"
+
+      "*.eld"
+      "*~"
+      "\\#*\\#"
+      ".dir-locals.el"
+
+      "**/.claude/settings.local.json"
+      "CLAUDE.md"
+    ];
+
+    settings = {
+      core.editor = "nvim";
+      user.name = "thelissimus";
+      user.email = "thelissimus@tuta.io";
+      init.defaultBranch = "master";
+      rerere.enabled = true;
+      diff.algorithm = "histogram";
+      merge.conflictStyle = "zdiff3";
+    };
+
+    signing = {
+      key = "464CA2FF1D27D92C";
+      signByDefault = true;
+    };
   };
+
   programs.difftastic = {
     enable = true;
     git.enable = true;
