876b0dad41d5e20f82590571f08d8ad61eaa3de2
thelissimus <thelissimus@tuta.io>
2026-03-17 05:52:35 +0500

    fix(adlr): home-manager global haskell installation environment

--- modules/home/haskell.nix
+++ modules/home/haskell.nix
@@ -2,19 +2,29 @@
 let
   h = pkgs.haskell.packages.ghc912;
   hpkgs = pkgs.haskellPackages;
+  htools = pkgs.buildEnv {
+    name = "haskell-tooling";
+    paths = [
+      h.cabal-install
+      h.haskell-language-server
+      hpkgs.cabal-fmt
+      h.fourmolu
+      h.hlint
+      h.hoogle
+      h.cabal-hoogle
+    ];
+    pathsToLink = [
+      "/bin"
+      "/share"
+    ];
+  };
 in
 {
   home.file.".ghci".source = ../../.config/.ghci;
 
   home.packages = [
     h.ghc
-    h.cabal-install
-    h.haskell-language-server
-    hpkgs.cabal-fmt
-    h.fourmolu
-    h.hlint
-    h.hoogle
-    # h.cabal-hoogle
+    htools
     # hpkgs.ghcprofview
   ];
 }
